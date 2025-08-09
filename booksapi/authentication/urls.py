0107430aa1c930b712bcc5ca93ad7086eaf6809b
ashique1213 <muhammedashique1213@gmail.com>
2025-08-09 17:34:07 +0530

    feat: Implement TokenObtainPairView for user login with JWT

--- booksapi/authentication/urls.py
+++ booksapi/authentication/urls.py
@@ -4,5 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('users/register/', UserRegistrationView.as_view(), name='user-register'),
+    path('users/login/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
    
 ]
